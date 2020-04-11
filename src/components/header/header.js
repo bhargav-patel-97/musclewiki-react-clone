70e4fb71fd1b5a34446a65339046d777a43dc8ef
bhargav <bhargavpatel6040@gmail.com>
2020-04-11 21:06:09 +0530

    Subject:  Commit initiated to save progress in order to eject. Message: This React project has been ejected to use CSS Modules.

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aux from '../../hoc/Auxilary';
+
+const header = (props) => (
+    <Aux>
+        <div className="heading">
+            <h1>MuscleWiki</h1>
+        </div>
+        <div className="navigation">
+        </div>
+    </Aux>
+);
+
+export default header;
