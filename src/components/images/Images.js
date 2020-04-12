64f0058407fdf104ae78d7dcbd9b3113c1ae6416
bhargav <bhargavpatel6040@gmail.com>
2020-04-12 23:28:15 +0530

    Subject: MuscleWiki React Clone//Adding Women figures and gender switching buttons. Message: Storing only required info on the state and including all required images for men and women in a seperate file. Still things are only limited to Desktop. Mobile Compatibility Remaining.

--- src/components/images/Images.js
+++ src/components/images/Images.js
@@ -1,5 +1,8 @@
-const musclewiki = 'https://musclewiki.org/static/Crops/'
-const images = [
+
+const musclewiki = 'https://musclewiki.org/static/Crops/';
+const musclewiki_female = 'https://musclewiki.org/static/Crops/female/';
+const images = { male: [
+
   { key: 0, src: `${musclewiki}00.-Blank-Figures.png`, className: 'background', alt: 'muscle-map-man' },
   { key: 1, src: `${musclewiki}08.-TrapsLeft.png`, className: 'trapsA', alt: 'muscle-map-man' },
   { key: 2, src: `${musclewiki}08.-TrapsRight.png`, className: 'trapsB', alt: 'muscle-map-man' },
@@ -32,11 +35,44 @@ const images = [
   { key: 29, src: `${musclewiki}13.C-Calves.png`, className: 'backcalvesA', alt: ''  },
   { key: 30, src: `${musclewiki}13.D-Calves.png`, className: 'backcalvesB', alt: ''  },
 
-];
-  
-  function imageLoader() {
-    return images;
-  }
+],
+female: [
+
+  { key: 0, src: `${musclewiki_female}Female-Figures.png`, className: 'background', alt: '' },
+  { key: 1, src: `${musclewiki_female}female-traps-A.png`, className: 'femaleTrapsA', alt: '' },
+  { key: 2, src: `${musclewiki_female}female-traps-B.png`, className: 'femaleTrapsB', alt: '' },
+  { key: 3, src: `${musclewiki_female}female-deltoids-A.png`, className: 'femaleShouldersA', alt: '' },
+  { key: 4, src: `${musclewiki_female}female-deltoids-B.png`, className: 'femaleShouldersB', alt: '' },
+  { key: 5, src: `${musclewiki_female}female-pecs.png`, className: 'femalePecs', alt: '' },
+  { key: 6, src: `${musclewiki_female}female-biceps-A.png`, className: 'femaleBicepsA', alt: '' },
+  { key: 7, src: `${musclewiki_female}female-biceps-B.png`, className: 'femaleBicepsB', alt: '' },
+  { key: 8, src: `${musclewiki_female}female-forearms-A.png`, className: 'femaleForearmA', alt: '' },
+  { key: 9, src: `${musclewiki_female}female-forearms-B.png`, className: 'femaleForearmB', alt: '' },
+  { key: 10, src: `${musclewiki_female}female-abdominals.png`, className: 'femaleAbdominals', alt: '' },
+  { key: 11, src: `${musclewiki_female}female-quads-A.png`, className: 'femaleQuadsA', alt: '' },
+  { key: 12, src: `${musclewiki_female}female-quads-B.png`, className: 'femaleQuadsB', alt: '' },
+  { key: 13, src: `${musclewiki_female}female-calves-A.png`, className: 'femaleCalvesA', alt: '' },
+  { key: 14, src: `${musclewiki_female}female-calves-B.png`, className: 'femaleCalvesB', alt: '' },
+  { key: 15, src: `${musclewiki_female}female-traps-C.png`, className: 'femaleBacktrapsA', alt: '' },
+  { key: 16, src: `${musclewiki_female}female-traps-D.png`, className: 'femaleBacktrapsB', alt: '' },
+  { key: 17, src: `${musclewiki_female}female-deltoids-C.png`, className: 'femaleBackShouldersA', alt: '' },
+  { key: 18, src: `${musclewiki_female}female-deltoids-D.png`, className: 'femaleBackShouldersB', alt: '' },
+  { key: 19, src: `${musclewiki_female}female-triceps-A.png`, className: 'femaleTricepsA', alt: '' },
+  { key: 20, src: `${musclewiki_female}female-triceps-B.png`, className: 'femaleTricepsB', alt: '' },
+  { key: 21, src: `${musclewiki_female}female-lats-A.png`, className: 'femaleBacklatsA', alt: '' },
+  { key: 22, src: `${musclewiki_female}female-lats-B.png`, className: 'femaleBacklatsB', alt: '' },
+  { key: 23, src: `${musclewiki_female}female-lowerback.png`, className: 'femaleBacklower', alt: '' },
+  { key: 24, src: `${musclewiki_female}female-forearms-C.png`, className: 'femaleBackForearmsA', alt: '' },
+  { key: 25, src: `${musclewiki_female}female-forearms-D.png`, className: 'femaleBackForearmsB', alt: '' },
+  { key: 26, src: `${musclewiki_female}female-glutes.png`, className: 'femaleBackGlutes', alt: '' },
+  { key: 27, src: `${musclewiki_female}female-hamstrings-A.png`, className: 'femaleBackHamstringsA', alt: '' },
+  { key: 28, src: `${musclewiki_female}female-hamstrings-B.png`, className: 'femaleBackHamstringsB', alt: '' },
+  { key: 29, src: `${musclewiki_female}female-calves-C.png`, className: 'femaleBackCalvesA', alt: '' },
+  { key: 30, src: `${musclewiki_female}female-calves-D.png`, className: 'femaleBackCalvesB', alt: '' },
   
 
-  export default imageLoader;
+]
+
+};
+  
+export default images;
--- src/containers/Musclemap/Musclemap.js
+++ src/containers/Musclemap/Musclemap.js
@@ -1,30 +1,63 @@
 import React from 'react';
-import './Musclemap.css'
-import imageLoader from '../../components/images/Images';
+import './musclemap_Male.css'
+import './musclemap_Female.css'
+import images from '../../components/images/Images';
 
 class Musclemap extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            images : [
-            ]
-         }
-        }
 
+    state = {
+        images : [],
+        selection: 'male',
+        Male: ['Male'],
+        Female: ['Female']
+    }
+            
+    femaleClickHandler = () => {
+    const maleBtn =  document.querySelector('.Male');
+    const femaleBtn =  document.querySelector('.Female');
+    maleBtn.classList.add('Inactive');
+    femaleBtn.classList.remove('Inactive');
+    
+    this.setState
+    ({
+        selection:'female',
+        images: images.female
+    });
+    }
+    
+    maleClickHandler = () => {
+    const maleBtn =  document.querySelector('.Male');
+    const femaleBtn =  document.querySelector('.Female');
+    maleBtn.classList.remove('Inactive');
+    femaleBtn.classList.add("Inactive");
+
+    this.setState
+    ({
+        selection:'male',
+        images: images.male
+    });
+    }
+        
     componentDidMount() {
-        const images = imageLoader()
-        this.setState({images})
-        console.log(this.state);
+        const initialImages = images.male;
+        this.setState({images: initialImages})
+        if(this.state.selection === 'male') {
+            this.setState({Female:['Female Inactive']});
+        }
     }
     
     render() {
-        return(
+        return([
+            <div className="genderBtn">
+                <button className={this.state.Male} onClick={this.maleClickHandler}>Male</button>
+                <button className={this.state.Female} onClick={this.femaleClickHandler}>Female</button>
+            </div>,
             <div className="Musclemap">
                 { this.state.images.map
                     (({key, className, src, alt}) => <img key={key} src={src} alt={alt} className={className}/>)
                 }
             </div>
-        );
+            ]);
     }
 
 }
