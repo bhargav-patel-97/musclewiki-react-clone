e40028c28aa456733b6fd1d6f7a09561654608ea
bhargav <bhargavpatel6040@gmail.com>
2020-04-15 15:39:44 +0530

    Subject: Fixing small typo.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './App.css';
-import Header from './components/header/Header';
+import Header from './components/header/header';
 import Musclemap from './containers/Musclemap/Musclemap';
 
 
